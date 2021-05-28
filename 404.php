99ddbc40dc14ae6c00864d175f903aa78e759ded
Lê Thị An Bình <73175396+lethianbinh@users.noreply.github.com>
2021-05-28 09:30:22 +0700

    Update 404

---
+++ 404.php
@@ -0,0 +1,25 @@
+<link href="https://fonts.googleapis.com/css?family=Montserrat:700,900" rel="stylesheet">
+<link rel="stylesheet" href="css/style404.css">
+
+
+<div id="notfound">
+    <div class="notfound">
+        <div class="notfound-404">
+            <h1>404</h1>
+            <h2>Page Not Gound</h2>
+        </div>
+        <a href="index.php">Homepage</a>
+    </div>
+</div>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-23581568-13');
+</script>
