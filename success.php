1ce3610bf150d78abc67e210b820fcc3b3005fd6
DinhNPVy <4501104282@student.hcmue.edu.vn>
2021-06-24 18:07:24 +0700

    upload success

--- success.php
+++ success.php
@@ -11,6 +11,9 @@ include 'inc/header.php';
 // }
 ?>
 
+<link rel="stylesheet" href="css/styledetails.css">
+<link rel="stylesheet" href="css/style.min.css">
+<link rel="stylesheet" href="css/materialdesignicons.min.css">
 <div class="container">
     <h1 style="color: green;">Success Order</h1>
     <?php
@@ -26,7 +29,7 @@ include 'inc/header.php';
     ?>
     <p>Total Price You Have Bought From My Website: <?php $vat = $amount * 0.2;
                                                     $total = $vat + $amount;
-                                                    echo $total;  ?> </p>
+                                                    echo $fm->format_currency($total);  ?> </p>
     <p>We will contact as soon as possiable. Please see your order details here <a href="orderdetail.php">Click Here!</a></p>
 </div>
 
