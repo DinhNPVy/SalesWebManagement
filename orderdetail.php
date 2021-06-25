a967d294a35a46390d5218ee8d9caa95487cd429
DinhNPVy <4501104282@student.hcmue.edu.vn>
2021-06-25 20:44:54 +0700

    upload orderdetail

--- orderdetail.php
+++ orderdetail.php
@@ -2,11 +2,36 @@
 include_once 'inc/header.php';
 ?>
 
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="css/bootstrap.css.map">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/styledetails.css">
+<link rel="stylesheet" href="css/style.min.css">
+<link rel="stylesheet" href="css/materialdesignicons.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="css/styleprofile.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
+
+
+
 <?php
+
+
 $login_check = Session::get("customer_signin");
 
 if ($login_check == false) {
     '<script>window.location=login.php</script>';
+}
+$ct = new cart();
+
+if (isset($_GET['confirmid'])) {
+    $id = $_GET['confirmid'];
+
+    $time = $_GET['time'];
+    $price = $_GET['price'];
+    $shifted_confirmid = $ct->ShiftedConfirmid($id, $time, $price);
 }
 
 ?>
@@ -70,7 +95,7 @@ if ($login_check == false) {
 
                                             <td><?php echo $result['productName'] ?></td>
                                             <td><img height="150px" src="admin/uploads/<?php echo $result['image'] ?>" alt=""></td>
-                                            <td><?php echo $result['price'] . ' ' . 'VNĐ' ?></td>
+                                            <td><?php echo $fm->format_currency($result['price']) ?></td>
                                             <td>
 
                                                 <?php echo $result['quantity'] ?>
@@ -82,8 +107,13 @@ if ($login_check == false) {
                                                 <?php
                                                 if ($result['status'] == '0') {
                                                     echo 'Pending';
+                                                } else if ($result['status'] == 1) {
+                                                ?>
+                                                    <span>Delivered</span>
+
+                                                <?php
                                                 } else {
-                                                    echo 'Processed';
+                                                    echo 'Received';
                                                 }
                                                 ?>
                                             </td>
@@ -93,10 +123,17 @@ if ($login_check == false) {
                                                 <td><?php echo 'N/A'  ?></td>
                                             <?php
 
-                                            } else {
+                                            } elseif ($result['status'] == 1) {
                                             ?>
-                                                <td><a href="?cartid=<?php echo $result['cartId'] ?>">Remove</a></td>
+                                                <td> <a href="?confirmid=<?php echo $customer_id ?>&price=<?php echo ($result['price']) . ''  ?>&time=<?php echo $result['date_order'] ?> ">Confirmed</a></td>
+                                            <?php
+                                            } else {
+
+                                            ?>
+                                                <td>
+                                                    <?php echo 'Received' ?>
                                                 </td>
+
                                             <?php
                                             }
                                             ?>
